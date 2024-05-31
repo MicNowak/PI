d8d01e2981774c08c03da5b25beda9efbdd66bdd
Michał Nowak <michalmikolajnowak36@gmail.com>
2024-05-31 20:52:06 +0200

    add padding to navigation

--- final/src/lib/components/Navigation.svelte
+++ final/src/lib/components/Navigation.svelte
@@ -50,13 +50,13 @@
 </script>
 
 <nav class="list-nav p-4">
-	<h4 class="h4">{pageInfo.main.header[lang]}</h4>
+	<h4 class="h4 py-1">{pageInfo.main.header[lang]}</h4>
 	<ul>
 		<li><a href="/" on:click={onClick}>{pageInfo.main.nav.home[lang]}</a></li>
 		<li><a href="/blog" on:click={onClick}>{pageInfo.main.nav.bloh[lang]}</a></li>
 		<li><a href="/about" on:click={onClick}>{pageInfo.main.nav.about[lang]}</a></li>
 	</ul>
-	<h4 class="h4">{pageInfo.projects.header[lang]}</h4>
+	<h4 class="h4 py-1">{pageInfo.projects.header[lang]}</h4>
 	<ul>
 		{#each pageInfo.projects.projects as project}
 			<li><a href={'/projects/' + project.path} on:click={onClick}>{project.name}</a></li>
